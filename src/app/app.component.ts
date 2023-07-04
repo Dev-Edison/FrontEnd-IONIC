f9832eb2e3729438e31f2fa628d127707aceb124
Edison Marcos <dev3dison@gmail.com>
2023-07-04 18:46:00 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+
+  public env = environment;
+
+  public appPages = [
+    { title: 'Início', url: '/home', icon: 'home' },
+    { title: 'Contatos', url: '/contacts', icon: 'chatbubbles' },
+    { title: 'Sobre', url: '/about', icon: 'information-circle' }
+  ];
+
+  public appUser = {
+    logged: false,
+    title: 'Login / Entrar',
+    url: '/login',
+    icon: 'log-in',
+    avatar: ''
+  }
+
+  constructor() { }
+}
